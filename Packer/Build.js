c967f9f2b77bbca99c5a7b14f796a132804aab24
1Y <arseniy.krugloff@gmail.com>
2022-07-05 04:42:47 +0300

    Commit

--- Packer/Build.js
+++ Packer/Build.js
@@ -14,11 +14,14 @@ const Regexps =
 
 
 
-function ParseIncludes(Code, Regexp)
+function Match(Code, Regexp)
 {
 	const Includes = [];
+
 	for (let Iter of Code.matchAll(Regexp))
-		Includes.push(Iter[1]);
+		if (!Includes.includes(Iter[1]))
+			Includes.push(Iter[1]);
+
 	return Includes;
 }
 
@@ -52,7 +55,7 @@ function Entry(Path, Regexp, Included = [])
 	}
 
 	const Code_This = Read(Path);
-	for (let Include of ParseIncludes(Code_This, /import '(.*?)'/g))
+	for (let Include of Match(Code_This, /import '(.*?)'/g))
 	{
 		const DependencyPath = _Path.join(_Path.dirname(Path), Include);
 		if (!Included.includes(DependencyPath))
@@ -79,7 +82,7 @@ function Lazy(Path, Regexp)
 	function R(Path)
 	{
 		const Code = ReadFile(Path);
-		const Includes = ParseIncludes(Code, Regexp).map(Value => _Path.resolve(Path, ('../' + Value)));
+		const Includes = Match(Code, Regexp).map(Value => _Path.resolve(Path, ('../' + Value)));
 
 		Swap(Includes, Path);
 
@@ -101,12 +104,14 @@ function Lazy(Path, Regexp)
 function GetSelectors(Code)
 {
 	const Selectors = [];
+
 	for (let Group of Code.matchAll(/<([^ \/>]+).*?>/g))
 	{
 		const Tag = Group[1];
 		if (!Selectors.includes(Tag))
 			Selectors.push(Tag);
 	};
+
 	return Selectors;
 }
 
@@ -165,32 +170,27 @@ function UglifyHTML(Code)
 
 
 
-function Icons()
+function GetIcons(Names)
 {
-	return `'Settings': "<path d='M19.43 12.98c.04-.32.07-.64.07-.98 0-.34-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.09-.16-.26-.25-.44-.25-.06 0-.12.01-.17.03l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65C14.46 2.18 14.25 2 14 2h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.06-.02-.12-.03-.18-.03-.17 0-.34.09-.43.25l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98 0 .33.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.09.16.26.25.44.25.06 0 .12-.01.17-.03l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.06.02.12.03.18.03.17 0 .34-.09.43-.25l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.65zm-1.98-1.71c.04.31.05.52.05.73 0 .21-.02.43-.05.73l-.14 1.13.89.7 1.08.84-.7 1.21-1.27-.51-1.04-.42-.9.68c-.43.32-.84.56-1.25.73l-1.06.43-.16 1.13-.2 1.35h-1.4l-.19-1.35-.16-1.13-1.06-.43c-.43-.18-.83-.41-1.23-.71l-.91-.7-1.06.43-1.27.51-.7-1.21 1.08-.84.89-.7-.14-1.13c-.03-.31-.05-.54-.05-.74s.02-.43.05-.73l.14-1.13-.89-.7-1.08-.84.7-1.21 1.27.51 1.04.42.9-.68c.43-.32.84-.56 1.25-.73l1.06-.43.16-1.13.2-1.35h1.39l.19 1.35.16 1.13 1.06.43c.43.18.83.41 1.23.71l.91.7 1.06-.43 1.27-.51.7 1.21-1.07.85-.89.7.14 1.13zM12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 6c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z'>",
-		'Devices': "<path d='M4 6h18V4H4c-1.1 0-2 .9-2 2v11H0v3h14v-3H4V6zm19 2h-6c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h6c.55 0 1-.45 1-1V9c0-.55-.45-1-1-1zm-1 9h-4v-7h4v7z'>",
-		'Note': "<path d='M14 17H4v2h10v-2zm6-8H4v2h16V9zM4 15h16v-2H4v2zM4 5v2h16V5H4z'>",
-		'Time': "<path d='M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm.5-13H11v6l5.25 3.15.75-1.23-4.5-2.67z'>",
-		'Error Outlined': "<path d='M11 15h2v2h-2v-2zm0-8h2v6h-2V7zm.99-5C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z'>",
-		'Chat Bubble': "<path d='M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H6l-2 2V4h16v12z'>",
-		'More Vert': "<path d='M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z'>",
-		'Arrow Back': "<path d='M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z'>",
-		'Bug': "<path d='M20 8h-2.81c-.45-.78-1.07-1.45-1.82-1.96L17 4.41 15.59 3l-2.17 2.17C12.96 5.06 12.49 5 12 5c-.49 0-.96.06-1.41.17L8.41 3 7 4.41l1.62 1.63C7.88 6.55 7.26 7.22 6.81 8H4v2h2.09c-.05.33-.09.66-.09 1v1H4v2h2v1c0 .34.04.67.09 1H4v2h2.81c1.04 1.79 2.97 3 5.19 3s4.15-1.21 5.19-3H20v-2h-2.09c.05-.33.09-.66.09-1v-1h2v-2h-2v-1c0-.34-.04-.67-.09-1H20V8zm-6 8h-4v-2h4v2zm0-4h-4v-2h4v2z'>",
-		'Server': "<path d='M20 13H4c-.55 0-1 .45-1 1v6c0 .55.45 1 1 1h16c.55 0 1-.45 1-1v-6c0-.55-.45-1-1-1zM7 19c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zM20 3H4c-.55 0-1 .45-1 1v6c0 .55.45 1 1 1h16c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1zM7 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z'>",
-		'Delete Forever': "<path d='M14.12 10.47L12 12.59l-2.13-2.12-1.41 1.41L10.59 14l-2.12 2.12 1.41 1.41L12 15.41l2.12 2.12 1.41-1.41L13.41 14l2.12-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4zM6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9z'>",
-		'Edit': "<path d='M14.06 9.02l.92.92L5.92 19H5v-.92l9.06-9.06M17.66 3c-.25 0-.51.1-.7.29l-1.83 1.83 3.75 3.75 1.83-1.83c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.2-.2-.45-.29-.71-.29zm-3.6 3.19L3 17.25V21h3.75L17.81 9.94l-3.75-3.75z'>",
-		'Wifi': "<path d='M1 9l2 2c4.97-4.97 13.03-4.97 18 0l2-2C16.93 2.93 7.08 2.93 1 9zm8 8l3 3 3-3c-1.65-1.66-4.34-1.66-6 0zm-4-4l2 2c2.76-2.76 7.24-2.76 10 0l2-2C15.14 9.14 8.87 9.14 5 13z'>",
-		'Pin': "<path d='M14,4v5c0,1.12,0.37,2.16,1,3H9c0.65-0.86,1-1.9,1-3V4H14 M17,2H7C6.45,2,6,2.45,6,3c0,0.55,0.45,1,1,1c0,0,0,0,0,0l1,0v5 c0,1.66-1.34,3-3,3v2h5.97v7l1,1l1-1v-7H19v-2c0,0,0,0,0,0c-1.66,0-3-1.34-3-3V4l1,0c0,0,0,0,0,0c0.55,0,1-0.45,1-1 C18,2.45,17.55,2,17,2L17,2z'>",
-		'Hide': "<path d='M12 6c3.79 0 7.17 2.13 8.82 5.5-.59 1.22-1.42 2.27-2.41 3.12l1.41 1.41c1.39-1.23 2.49-2.77 3.18-4.53C21.27 7.11 17 4 12 4c-1.27 0-2.49.2-3.64.57l1.65 1.65C10.66 6.09 11.32 6 12 6zm-1.07 1.14L13 9.21c.57.25 1.03.71 1.28 1.28l2.07 2.07c.08-.34.14-.7.14-1.07C16.5 9.01 14.48 7 12 7c-.37 0-.72.05-1.07.14zM2.01 3.87l2.68 2.68C3.06 7.83 1.77 9.53 1 11.5 2.73 15.89 7 19 12 19c1.52 0 2.98-.29 4.32-.82l3.42 3.42 1.41-1.41L3.42 2.45 2.01 3.87zm7.5 7.5l2.61 2.61c-.04.01-.08.02-.12.02-1.38 0-2.5-1.12-2.5-2.5 0-.05.01-.08.01-.13zm-3.4-3.4l1.75 1.75c-.23.55-.36 1.15-.36 1.78 0 2.48 2.02 4.5 4.5 4.5.63 0 1.23-.13 1.77-.36l.98.98c-.88.24-1.8.38-2.75.38-3.79 0-7.17-2.13-8.82-5.5.7-1.43 1.72-2.61 2.93-3.53z'>",
-		'Date Range': "<path d='M9 11H7v2h2v-2zm4 0h-2v2h2v-2zm4 0h-2v2h2v-2zm2-7h-1V2h-2v2H8V2H6v2H5c-1.11 0-1.99.9-1.99 2L3 20c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V9h14v11z'>",
-		'Chevron Left': "<path d='M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z'>",
-		'Chevron Right': "<path d='M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z'>",
-		'Link': "<path d='M17 7h-4v2h4c1.65 0 3 1.35 3 3s-1.35 3-3 3h-4v2h4c2.76 0 5-2.24 5-5s-2.24-5-5-5zm-6 8H7c-1.65 0-3-1.35-3-3s1.35-3 3-3h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-2zm-3-4h8v2H8z'>",
-		'Link Off': "<path d='M14.39 11L16 12.61V11zM17 7h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1 0 1.27-.77 2.37-1.87 2.84l1.4 1.4C21.05 15.36 22 13.79 22 12c0-2.76-2.24-5-5-5zM2 4.27l3.11 3.11C3.29 8.12 2 9.91 2 12c0 2.76 2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1 0-1.59 1.21-2.9 2.76-3.07L8.73 11H8v2h2.73L13 15.27V17h1.73l4.01 4.01 1.41-1.41L3.41 2.86 2 4.27z'>",
-		'Checkbox-Unchecked': "<path d='M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'/>",
-		'Checkbox-Checked': "<path d='M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.11 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm-9 14l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z'/>",
-		'Linear Scale': "<path d='M19.5 9.5c-1.03 0-1.9.62-2.29 1.5h-2.92c-.39-.88-1.26-1.5-2.29-1.5s-1.9.62-2.29 1.5H6.79c-.39-.88-1.26-1.5-2.29-1.5C3.12 9.5 2 10.62 2 12s1.12 2.5 2.5 2.5c1.03 0 1.9-.62 2.29-1.5h2.92c.39.88 1.26 1.5 2.29 1.5s1.9-.62 2.29-1.5h2.92c.39.88 1.26 1.5 2.29 1.5 1.38 0 2.5-1.12 2.5-2.5s-1.12-2.5-2.5-2.5z'/>",
-		'code': "<Path D='M9.4 16.6L4.8 12l4.6-4.6L8 6l-6 6 6 6 1.4-1.4zm5.2 0l4.6-4.6-4.6-4.6L16 6l6 6-6 6-1.4-1.4z'/>"`;
+	const Output = {};
+
+	const Promises = [];
+	for (let Name of Names)
+		Promises.push(new Promise((Resolve) =>
+		{
+			FS.readFile(`node_modules/@material-design-icons/svg/outlined/${Name}.svg`, (Error, Data) =>
+			{
+				Output[Name] = Data.toString().replace(`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">`, '').replace('</svg>', '');
+				Resolve();
+			});
+		}));
+
+	return new Promise((Resolve) => Promise.all(Promises).then(() => Resolve(Output)));
+}
+
+function GetIconsList(Code)
+{
+	return Match(Code, /Icon="(.*?)"/g);
 }
 
 function Clear()
@@ -219,23 +219,28 @@ function PackCSS()
 
 function PackJS()
 {
-	WriteFile
-	(
-		Output + '/Script.js',
-		UglifyJS
+	const Icons = GetIcons([...GetIconsList(ReadFile('../Source/Index.html')),  'check_box_outline_blank', 'check_box', 'more_vert', 'expand_more', 'expand_less']);
+
+	Promise.all([Icons]).then((Values) =>
+	{
+		WriteFile
 		(
-			Merge
-			([
-				...Lazy(`${Library}/**/*.js`, Regexps.JS),
-				...Lazy('../Source/*.js', Regexps.JS)
-			])
-			.replace(Regexps.JS, '')
-			.replace(`// ...Generator puts icons here...`, Icons(['remove_forever']))
-		)
-		.replace(/>( |\n|\t)*?</g, '><')
-		.replace(/ +/g, ' ')
-		.replace(/\t/g, '')
-	);
+			Output + '/Script.js',
+			UglifyJS
+			(
+				Merge
+				([
+					...Lazy(`${Library}/**/*.js`, Regexps.JS),
+					...Lazy('../Source/*.js', Regexps.JS)
+				])
+				.replace(Regexps.JS, '')
+				.replace(`{ /* ...Generator puts icons here... */ }`, JSON.stringify(Values[0]))
+			)
+			.replace(/>( |\n|\t)*?</g, '><')
+			.replace(/ +/g, ' ')
+			.replace(/\t/g, '')
+		);
+	})
 }
 
 function PackHTML()
--- Source/Theme.js
+++ Source/Theme.js
@@ -1,16 +1,31 @@
-function Theme_SetColor_Main(Hex)
+function Theme_Set_Main(Hex)
 {
 	const HSL = Hex_To_HSL(Hex);
+
 	document.documentElement.style.setProperty('--Hue', HSL[0]);
 	document.documentElement.style.setProperty('--Saturation', HSL[1] + '%');
 	document.documentElement.style.setProperty('--Lightness', HSL[2] + '%');
+
+	document.getElementById('Theme_Code_Main').innerHTML = `:root <BR>
+										  { <BR>
+										      --Hue: ${HSL[0]}; <BR>
+										      --Saturation: ${HSL[1]}%; <BR>
+										      --Lightness: ${HSL[2]}%; <BR>
+										  }`;
 }
 
-function Theme_SetColor_Gray(Hex)
+function Theme_Set_Gray(Hex)
 {
 	const HSL = Hex_To_HSL(Hex);
+
 	document.documentElement.style.setProperty('--Gray-Hue', HSL[0]);
 	document.documentElement.style.setProperty('--Gray-Saturation', HSL[1] + '%');
+
+	document.getElementById('Theme_Code_Gray').innerHTML = `:root <BR>
+										  { <BR>
+										      --Gray-Hue: ${HSL[0]}; <BR>
+										      --Gray-Saturation: ${HSL[1]}%; <BR>
+										  }`;
 }
 
 function Theme_Reset_Main()
