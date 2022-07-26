5c160672fd1a892c21534f9dc5d3b15846581e4a
1Y <arseniy.krugloff@gmail.com>
2022-07-26 17:40:22 +0300

    Commit

---
+++ Dialog.js
@@ -0,0 +1,9 @@
+function Demo_Dialog_FullScreen_Open()
+{
+	window.Demo_Dialog_FullScreen = new Dialog_FullScreen();
+}
+
+function Demo_Dialog_FullScreen_Close()
+{
+	window.Demo_Dialog_FullScreen.Close();
+}
