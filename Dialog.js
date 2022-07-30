08d7e69056133d673b275879250c0f61a811d327
1Y <arseniy.krugloff@gmail.com>
2022-07-31 02:08:44 +0300

    Commit

--- Dialog.js
+++ Dialog.js
@@ -1,9 +1,22 @@
 function Demo_Dialog_FullScreen_Open()
 {
 	window.Demo_Dialog_FullScreen = new Dialog_FullScreen();
+	window.Demo_Dialog_FullScreen.Animated = true;
+	window.Demo_Dialog_FullScreen.Element.innerHTML =
+	`
+		<Div AutoLayout Direction="Horizontal" Width="Fill" Style="Padding: 25px" OnClick="Demo_Dialog_FullScreen_Close()">
+			<Custom-round-button Icon="outlined/arrow_back" FocusFirst></Custom-round-button>
+		</Div>
+
+		<Div AutoLayout Direction="Vertical" Width="Fill" Height="Fill" AlignX="Center" AlignY="Center" Style="Gap: 50px; Padding: 25px;">
+			<H2 Style="Text-align: Center">Полноэкранно!</H2>
+		</Div>
+	`;
+	window.Demo_Dialog_FullScreen.Open();
 }
 
 function Demo_Dialog_FullScreen_Close()
 {
 	window.Demo_Dialog_FullScreen.Close();
+	delete window.Demo_Dialog_FullScreen;
 }
--- FAB.js
+++
@@ -1,5 +0,0 @@
-function FAB_Float(Checked)
-{
-	const Button = document.getElementById('FAB_Content').children[0];
-	Button.classList.toggle('Locked', !Checked);
-}
